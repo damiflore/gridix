1182a1b8021f9c77c1f2738e112cea278d1745c2
Damien Maillard <damien.maillard@dailymotion.com>
2021-01-22 17:05:47 +0100

    add a phaser example

--- jsenv.config.js
+++ jsenv.config.js
@@ -1,4 +1,4 @@
-import { jsenvBabelPluginMap } from "@jsenv/core"
+import { jsenvBabelPluginMap, convertCommonJsWithRollup } from "@jsenv/core"
 
 export const projectDirectoryUrl = String(new URL("./", import.meta.url))
 
@@ -7,4 +7,8 @@ export const importMapFileRelativeUrl = `importmap.prod.importmap`
 export const babelPluginMap = {
   ...jsenvBabelPluginMap,
   // add babel plugin if needed
+}
+
+export const convertMap = {
+  "./node_modules/phaser/src/phaser.js": convertCommonJsWithRollup,
 }
